942ffafdda81219828863eb1a3b4861c6c6638c5
edubuntu <ed@123.com>
2023-06-27 08:35:43 +0530

    second commit of simpled.java

--- simpled.java
+++ simpled.java
@@ -1,6 +1,6 @@
 class Simple{
     public static void main(String args[]){
-    System.out.println("Hello Git, Welcome to Online Training");
+    System.out.println("Hi Git user, Welcome to Online Training");
     }
 }
 
